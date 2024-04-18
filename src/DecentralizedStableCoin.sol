07bbb2c41e9b1c6a877062a6fba4f5a7e6f7f44c
Paulo Vidal <vidalplr@outlook.com>
2024-04-17 22:46:40 -0300

    chore: initial commit

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+/*
+ * @title Decentralized Stable Coin
+ * @author vidalpaul
+ * @dev Collateral: Exogenous (wETH and wBTC)
+ * @dev Minting: Algorithmic
+ * @dev Relative Stability: 1:1 USD
+ *
+ * @notice This contract is meant to be governed by DSCEngine. This contract is just the ERC20 implementation of the stablecoin system.
+ */
+contract DecentralizedStableCoin {
+    constructor() {}
+}
